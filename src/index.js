e0abd4cd5dbcfe5e03342a1490297322327e14cf
Lee Chuan <hyde.lee.chuan@gmail.com>
2019-06-22 13:11:41 +0800

    updated for all bugs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const feature1 = require('./feature1');
 
 const app = express();
-const server = app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const server = app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
 
 app.get('/', (req, res) => {
   res.send(`Hello ${feature1.registerName(req)}`);
